a3a4b8a7705b2bf782fc6a251e256395d29307ac
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-17 11:55:46 +0200

    add converter factory

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,6 +1,7 @@
 package com.example.marsphotos.network
 
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 // URL base del servicio web
 private const val BASE_URL =
@@ -8,3 +9,7 @@ private const val BASE_URL =
 
 // compilador de Retrofit para compilar y crear un objeto Retrofit
 private val retrofit = Retrofit.Builder()
+    // fábrica de conversión para crear una API de servicios web
+    // el conversor indica a Retrofit qué hacer con los datos que obtiene del servicio web
+    // queremos recuperar una respuesta JSON del servicio web y mostrarla como String
+    .addConverterFactory(ScalarsConverterFactory.create())
