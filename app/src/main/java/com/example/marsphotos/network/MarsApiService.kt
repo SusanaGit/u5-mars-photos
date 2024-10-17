b4a9fc08f5dbb1a38b3185acfc2b878c496e15b4
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-17 11:43:18 +0200

    create val retrofit

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,5 +1,10 @@
 package com.example.marsphotos.network
 
+import retrofit2.Retrofit
+
 // URL base del servicio web
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
+
+// compilador de Retrofit para compilar y crear un objeto Retrofit
+private val retrofit = Retrofit.Builder()
